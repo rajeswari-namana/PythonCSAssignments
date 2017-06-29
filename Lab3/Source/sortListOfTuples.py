6eb4f90cf3c8a024add89d93e164d6d1598f9749
rajeswari-namana <raji.namana@gmail.com>
2017-06-29 17:33:09 -0500

    Lab 3 files organised

--- Lab3/Source/sortListOfTuples.py
+++ Lab3/Source/sortListOfTuples.py
@@ -1,12 +1,12 @@
 import operator
 
 #initialize a list
-list1=[(1,2),(3,4),(6,3),(2,1)]
+list1=[(1,2),(3,4),(6,3,5),(2,1)]
 
 '''sort is given a parameter called key, to tell it to perform sort 
 on key and we are using itemgetter method of operator package
 for that purpose.'''
-list1.sort(key=operator.itemgetter(1))
+list1.sort(key=operator.itemgetter(-1))
 
 #print sorted list
 print(list1)
